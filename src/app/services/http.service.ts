ee04e20030f47e9062f6f4667b40308f48bbca43
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-02-27 00:31:56 +0100

    add method to return observable with data response simulation from API

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,8 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserData } from '../data-structure-definitions/user-data';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpService {
-  constructor() {}
+  private URL = '../assets/API.json';
+
+  constructor(private http: HttpClient) {}
+
+  //return Observable with Get UserData request with API response simulation
+  getUserData(): Observable<UserData> {
+    return this.http.get<UserData>(this.URL);
+  }
 }
