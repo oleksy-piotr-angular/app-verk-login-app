2e1ab407b8d6ab2b606f3e84bf4797c5ae5a115c
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-02-26 14:13:08 +0100

    Add user data Definition Interface

---
+++ src/app/data-structure-definitions/user-data.ts
@@ -0,0 +1,23 @@
+// Definition of the structure of the user data
+export interface UserData {
+  id: number;
+  name: string;
+  username: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
