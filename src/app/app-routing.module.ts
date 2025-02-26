674a3a07b996bcb2afd30ec506785ad970fe6257
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-02-26 23:53:41 +0100

    guard has been set to prevent unauthorized visit on the home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { canOpenIfLoggedInGuard } from './guards/can-open-if-logged-in.guard';
 
 const routes: Routes = [
-  { path: 'login', component: LoginPageComponent },
-  { path: 'home', component: HomePageComponent },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'home',
+    component: HomePageComponent,
+    canActivate: [canOpenIfLoggedInGuard], ///guard has been set to prevent unauthorized opening of the home page
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // redirect to `home page` when root is opening
 ];
 
 @NgModule({
--- src/app/guards/can-open-if-logged-in.guard.ts
+++ src/app/guards/can-open-if-logged-in.guard.ts
@@ -2,6 +2,7 @@ import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
+//guard to prevent unauthorized opening of a given route path
 export const canOpenIfLoggedInGuard: CanActivateFn = () => {
-  return true;
+  return inject(AuthService).isAuthenticated();
 };
