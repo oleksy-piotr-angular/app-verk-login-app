b6dbf42c953e985213b4b489e3f54996f477cf55
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-02-27 13:28:10 +0100

    add a component with a control to display errors that appear in form controls

--- src/app/components/error-message-control/error-message-control.component.ts
+++ src/app/components/error-message-control/error-message-control.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-error-message-control',
   templateUrl: './error-message-control.component.html',
-  styleUrl: './error-message-control.component.css'
+  styleUrl: './error-message-control.component.css',
 })
-export class ErrorMessageControlComponent {
+export class ErrorMessageControlComponent implements OnChanges {
+  @Input() control: ValidationErrors | null = null;
+  errorMessages: string[] = [];
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['control'] && this.control) {
+      this.errorMessages = this.getErrorMessages(this.control);
+    }
+  }
+
+  private getErrorMessages(errors: ValidationErrors): string[] {
+    const messages: string[] = [];
+    for (const errorName in errors) {
+      if (errors.hasOwnProperty(errorName)) {
+        switch (errorName) {
+          case 'required':
+            messages.push('This field is required. ');
+            break;
+          case 'isEmailInvalid':
+            messages.push(
+              `The email address is invalid. Please enter a valid email address. `
+            );
+            break;
+          default:
+            messages.push('Invalid field.');
+            break;
+        }
+      }
+    }
+    return messages;
+  }
 }
