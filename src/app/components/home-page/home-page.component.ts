c6f60e492ec441243e475d3deef02f3dde35447d
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-02-27 12:13:16 +0100

    show user Data on Home Page

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,13 +1,46 @@
+import { Subscription } from 'rxjs';
+import { HttpService } from '../../services/http.service';
 import { AuthService } from './../../services/auth.service';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserData } from '../../data-structure-definitions/user-data';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
-export class HomePageComponent {
-  constructor(private authService: AuthService) {}
+export class HomePageComponent implements OnInit, OnDestroy {
+  userData: UserData | null = null;
+  userEmail: string | null = null;
+  getUserDataSub: Subscription | null = null;
+  userEmailSub: Subscription | null = null;
+  constructor(
+    private authService: AuthService,
+    private httpService: HttpService
+  ) {}
+
+  ngOnInit() {
+    //get user data from API when the component is initialized
+    this.getUserDataSub = this.httpService.getUserData().subscribe((data) => {
+      this.userData = data;
+    });
+    //get userEmail which was used to login
+    this.userEmailSub = this.authService.getUserEmail().subscribe((email) => {
+      this.userEmail = email;
+    });
+  }
+
+  ngOnDestroy(): void {
+    //unsubscribe from the getUserDataSub subscription when the component is destroyed
+    if (this.getUserDataSub) {
+      this.getUserDataSub.unsubscribe();
+    }
+    //unsubscribe from the userEmailSub subscription when the component is destroyed
+    if (this.userEmailSub) {
+      this.userEmailSub.unsubscribe();
+    }
+  }
+
   //for the logout button click event
   onLogout() {
     this.authService.logout();
