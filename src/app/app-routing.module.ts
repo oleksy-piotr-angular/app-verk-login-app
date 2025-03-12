a4a0d321db90076844fa05775aee1f765992c5f9
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-03-12 10:20:20 +0100

    change elements order in "routes" array

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { canOpenIfLoggedInGuard } from './guards/can-open-if-logged-in.guard';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // redirect to `home page` when root is opening
   {
     path: 'login',
     component: LoginPageComponent,
@@ -14,7 +15,6 @@ const routes: Routes = [
     component: HomePageComponent,
     canActivate: [canOpenIfLoggedInGuard], ///guard has been set to prevent unauthorized opening of the home page
   },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }, // redirect to `home page` when root is opening
 ];
 
 @NgModule({
