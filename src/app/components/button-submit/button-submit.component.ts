961eb8c0b19d0dd37b9181be92337b317916a892
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-03-19 23:55:30 +0100

    create "button-submit" component for NgForm

--- src/app/components/button-submit/button-submit.component.ts
+++ src/app/components/button-submit/button-submit.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { ControlContainer, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-button-submit',
   templateUrl: './button-submit.component.html',
-  styleUrl: './button-submit.component.scss'
+  styleUrl: './button-submit.component.scss',
+  providers: [{ provide: ControlContainer, useExisting: NgForm }],
 })
 export class ButtonSubmitComponent {
-
+  constructor(public control: NgForm) {}
 }
