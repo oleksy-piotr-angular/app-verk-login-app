9a5105b9922778e6fe291158850c41fda5ad07ff
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-03-20 00:33:26 +0100

    Additional codintion if we check Email with "appCheckEmail" validator Directive

--- src/app/directives/check-email.directive.ts
+++ src/app/directives/check-email.directive.ts
@@ -18,15 +18,16 @@ import {
 })
 //custom directive to validate email with Regular Expression
 export class CheckEmailDirective implements Validator {
-  @Input('appCheckEmail') checkEmail: boolean = false;
+  @Input('appCheckEmail') checkEmail: boolean = false; //validation check is disabled by default
   validate(control: AbstractControl): ValidationErrors | null {
     const EMAIL_REGEXP = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
-    const isEmailInvalid =
-      EMAIL_REGEXP.test(control.value) && this.checkEmail
-        ? null
-        : { isEmailInvalid: true };
+    //If Not Check Email do nothing and return null
+    if (!this.checkEmail) {
+      return null;
+    }
 
-    return isEmailInvalid;
+    //Test Email with Regular Expression and return error if not match
+    return EMAIL_REGEXP.test(control.value) ? null : { isEmailInvalid: true };
   }
 }
