7efa5fd624e677d7f1b94a82ddc7716ae1a8c78f
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-03-20 01:33:15 +0100

    I created a custom input-control component and implemented it in the project | I need to think how to properly emit data to the parent form component.

--- src/app/components/input-control/input-control.component.ts
+++ src/app/components/input-control/input-control.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { InputControlAttrData } from '../../data-structure-definitions/input-control-attr-data';
+import { ControlContainer, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-input-control',
   templateUrl: './input-control.component.html',
-  styleUrl: './input-control.component.scss'
+  styleUrl: './input-control.component.scss',
+  viewProviders: [{ provide: ControlContainer, useExisting: NgForm }],
 })
 export class InputControlComponent {
+  @Input('input-control') public controlAttrData!: InputControlAttrData;
+  @Output() public valueEmitter: EventEmitter<string> =
+    new EventEmitter<string>();
+  ngOnInit(): void {
+    console.log(this.controlAttrData);
+  }
 
+  onChange(value: string | undefined): void {
+    console.log(value);
+  }
 }
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { InputControlAttrData } from '../../data-structure-definitions/input-control-attr-data';
 
 @Component({
   selector: 'app-login-page',
@@ -10,6 +11,7 @@ import { Router } from '@angular/router';
 export class LoginPageComponent implements OnInit {
   public email: string = '';
   public password: string = '';
+  public inputControls: InputControlAttrData[] = [];
 
   public constructor(private auth: AuthService, private router: Router) {}
 
@@ -18,11 +20,31 @@ export class LoginPageComponent implements OnInit {
     if (this.auth.isAuthenticated()) {
       this.router.navigate(['/home']);
     }
+    //initialize controls for login form
+    this.inputControls = [
+      {
+        label: 'email',
+        type: 'email',
+        placeholder: 'Enter email',
+        isRequired: true,
+        checkEmail: true,
+      },
+      {
+        label: 'password',
+        type: 'password',
+        placeholder: 'Enter password',
+        isRequired: true,
+        checkEmail: false,
+      },
+    ];
   }
 
   public onSubmit(formValue: {
     login: { email: string; password: string };
   }): void {
     this.auth.login(formValue.login.email, formValue.login.password);
+    this.inputControls.forEach((control) => {
+      console.log('controls: ' + control.checkEmail + control.value);
+    });
   }
 }
