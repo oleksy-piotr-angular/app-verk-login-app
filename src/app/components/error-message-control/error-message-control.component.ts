2eaee9007c12c486e6e80ff2e39d7543999062e7
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-05-29 15:47:08 +0200

    changed the error message control to correctly display form validation errors for each input

--- src/app/components/error-message-control/error-message-control.component.ts
+++ src/app/components/error-message-control/error-message-control.component.ts
@@ -1,44 +1,54 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { ValidationErrors } from '@angular/forms';
+import { Component, Input } from '@angular/core';
+import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-error-message-control',
   templateUrl: './error-message-control.component.html',
-  styleUrl: './error-message-control.component.scss',
+  styleUrls: ['./error-message-control.component.scss'],
 })
-export class ErrorMessageControlComponent implements OnChanges {
-  @Input('errors') set setErrors(_errors: ValidationErrors | null) {
-    this.validationErrors = _errors;
-  }
+export class ErrorMessageControlComponent {
+  @Input() public control?: AbstractControl; // Input for the FormControl
 
-  public errorMessages: string[] = [];
-  private validationErrors: ValidationErrors | null = null;
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['setErrors'] && this.validationErrors) {
-      this.errorMessages = this.getErrorMessages(this.validationErrors);
-    }
-  }
-
-  private getErrorMessages(errors: ValidationErrors): string[] {
+  // Method to retrieve error messages based on validation errors
+  get errorMessages(): string[] {
+    const errors: ValidationErrors | null = this.control?.errors || null;
     const messages: string[] = [];
-    for (const errorName in errors) {
-      if (errors.hasOwnProperty(errorName)) {
-        switch (errorName) {
-          case 'required':
-            messages.push('This field is required. ');
-            break;
-          case 'isEmailInvalid':
-            messages.push(
-              `The email address is invalid. Please enter a valid email address. `
-            );
-            break;
-          default:
-            messages.push('Invalid field.');
-            break;
+
+    if (errors) {
+      for (const errorName in errors) {
+        if (errors.hasOwnProperty(errorName)) {
+          switch (errorName) {
+            case 'required':
+              messages.push('This field is required.');
+              break;
+            case 'emailFormat':
+              messages.push('Invalid email format.');
+              break;
+            case 'passwordStrength':
+              const error = errors['passwordStrength'];
+              if (!error.hasUpperCase)
+                messages.push(
+                  'Password must contain at least one uppercase letter.'
+                );
+              if (!error.hasLowerCase)
+                messages.push(
+                  'Password must contain at least one lowercase letter.'
+                );
+              if (!error.hasNumber)
+                messages.push(
+                  'Password must contain at least one numeric digit.'
+                );
+              if (!error.hasMinLength)
+                messages.push('Password must be at least 8 characters long.');
+              break;
+            default:
+              messages.push('Invalid field.');
+              break;
+          }
         }
       }
     }
+
     return messages;
   }
 }
