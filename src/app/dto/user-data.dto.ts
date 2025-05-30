2dd78180f27cac6cfbd9d07032b2503ba25d78f0
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-05-30 21:54:30 +0200

    adding DTO UserData class

---
+++ src/app/dto/user-data.dto.ts
@@ -0,0 +1,24 @@
+export class UserDataDTO {
+  constructor(
+    public id: number,
+    public name: string,
+    public username: string,
+    public address: {
+      street: string;
+      suite: string;
+      city: string;
+      zipcode: string;
+      geo: {
+        lat: string;
+        lng: string;
+      };
+    },
+    public phone: string,
+    public website: string,
+    public company: {
+      name: string;
+      catchPhrase: string;
+      bs: string;
+    }
+  ) {}
+}
