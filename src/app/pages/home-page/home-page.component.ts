b1ba3d70ae65ea70dc0dae2f1e51f20ddd2900b9
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-05-30 21:24:48 +0200

    rename directory "data-structure-definitions" to "models"

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -2,7 +2,7 @@ import { Subscription } from 'rxjs';
 import { HttpService } from '../../services/http.service';
 import { AuthService } from './../../services/auth.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { UserData } from '../../data-structure-definitions/user-data';
+import { UserData } from '../../models/user-data';
 import { Router } from '@angular/router';
 
 @Component({
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { UserData } from '../data-structure-definitions/user-data';
+import { UserData } from '../models/user-data';
 
 @Injectable({
   providedIn: 'root',
